d8c4f2184f41e01085954023b9947280f279d41a
Whitney Monk <whitneym92@gmail.com>
2020-06-17 16:28:52 -0500

    created my variables

---
+++ script.js
@@ -0,0 +1,20 @@
+/*Creating my variables*/
+$(document).ready(function() {
+    var APIKey= "cc3550ca747909c191efae6397fdaa94";
+    var cityDisplayEl = document.querySelector(".city-holder");
+    var inputEl = document.getElementById("city-input");
+    var searchEl = document.getElementById("search");
+    var currentCityEl = document.getElementById("current-city");
+    var currentDateEl = document.getElementById("current-date");
+    var tempEl= document.getElementById("temp");
+    var humidityEl= document.getElementById("humidity");
+    var windEl= document.getElementById("wind");
+    var uvEl= document.getElementById("uv-index");
+    var iconEl= document.getElementById("weather.icon");
+    var forecastDivs = document.querySelectorAll(".forecast");
+    var title = document.querySelector("title");
+
+    
+
+
+}
